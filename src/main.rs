44587ce526af2208ddc3df0c093229ef7f82a3c3
Brian Pfretzschner <brian.pfretzschner@gmail.com>
2021-07-06 22:31:54 +0200

    Itegrate rocket and diesl sqlite connection pool

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,31 @@
+#[macro_use] extern crate rocket;
+
+use dotenv::dotenv;
+use rocket_sync_db_pools::diesel;
+use rocket_sync_db_pools::database;
+
 mod cl_args;
-mod data_store;
 mod source_images;
 
-use crate::data_store::DataStore;
+#[database("db")]
+struct Database(diesel::SqliteConnection);
 
-fn main() {
+#[get("/")]
+fn hello(mut conn: Database) -> &'static str {
+    "Hello, world!"
+}
+
+#[rocket::main]
+async fn main() {
+    dotenv().ok();
     env_logger::init();
+
     let args = cl_args::parse_args().unwrap();
-    let data_store = DataStore::open(&args.data_root).unwrap();
     let source_image = source_images::search(&args.image_root);
 
-    // for source_image in source_image {
-    //     println!("Found image: {:?}", source_image);
-    // }
+    rocket::build()
+        .attach(Database::fairing())
+        .mount("/hello", routes![hello])
+        .launch()
+        .await.unwrap();
 }
