e885100dafa28205b239b4c7a7b19f63e508c0ef
Brian Pfretzschner <brian.pfretzschner@de.bosch.com>
2022-01-16 19:30:26 +0100

    feat: add view image feature on flashbacks

--- webapp/src/app/flashback/flashback.component.ts
+++ webapp/src/app/flashback/flashback.component.ts
@@ -1,31 +1,41 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MediaPresenterService } from '../media-presenter/media-presenter.service';
+import { FlashbackMediaPresentation } from './flashback-media-presentation';
 
 type FlashbackDates = string[];
-type DateImageIds = { [date: string]: number[]; };
+type DateImageIds = { [date: string]: number[] };
 
 @Component({
   selector: 'app-flashback',
   templateUrl: './flashback.component.html',
-  styleUrls: ['./flashback.component.scss']
+  styleUrls: ['./flashback.component.scss'],
 })
 export class FlashbackComponent implements OnInit {
-
   flashbackDates: FlashbackDates = null;
   dateImageIds: DateImageIds = null;
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(
+    private http: HttpClient,
+    private mediaPresenterService: MediaPresenterService
+  ) {}
 
   ngOnInit(): void {
-    this.http.get('/api/flashback/dates')
-      .subscribe(dateImageIds => {
-        this.flashbackDates = Object.keys(dateImageIds).reverse();
-        this.dateImageIds = dateImageIds as DateImageIds;
-      });
+    this.http.get('/api/flashback/dates').subscribe((dateImageIds) => {
+      this.flashbackDates = Object.keys(dateImageIds).reverse();
+      this.dateImageIds = dateImageIds as DateImageIds;
+    });
   }
 
-  hasNoFlashbacks() {
-    return this.flashbackDates !== null && this.flashbackDates.length === 0;
+  hasFlashbacks() {
+    return this.flashbackDates !== null && this.flashbackDates.length > 0;
+  }
+
+  onImageClick(date: string, imageId: number) {
+    const presentation = new FlashbackMediaPresentation(
+      this.dateImageIds[date],
+      imageId
+    );
+    this.mediaPresenterService.startPresentation(presentation);
   }
 }
